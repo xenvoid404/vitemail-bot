a33ddb861640377e1ec83cc80e8afc428af1c30b
xenvoid404 <xenvoid404@gmail.com>
2026-04-18 15:09:21 +0700

    hapus container docker redis

--- src/config/env.ts
+++ src/config/env.ts
@@ -22,6 +22,7 @@ export default {
         host: process.env.REDIS_HOST || 'localhost',
         password: process.env.REDIS_PASSWORD || undefined,
         port: Number(process.env.REDIS_PORT) || 6379,
+        keyPrefix: process.env.REDIS_PREFIX,
         maxRetriesPerRequest: null,
         enableReadyCheck: false,
     },
